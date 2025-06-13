15fcffc4cb552695181036a0c74747f8add501f8
CatAvadani <catalinaavadani@gmail.com>
2025-06-13 21:33:50 +0200

    Initialize project with Expo, React Navigation, TypeScript and Tailwind CSS (Nativewind) configuration for styling.

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function SettingsScreen() {
+  return (
+    <View className='flex-1 items-center justify-center bg-white dark:bg-gray-900'>
+      <Text className=' text-3xl font-bold'>Settings Screen</Text>
+    </View>
+  );
+}
